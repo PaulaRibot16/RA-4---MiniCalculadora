5eb56aea1564049c8b2cd41c482ceea77c20ff9e
paula <pribros1503@g.educaand.es>
2026-04-06 12:18:48 +0200

    Raaiz Cuadrada

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -2,4 +2,8 @@ public class MiniCalculadoraEjemplo {
     public double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
+
+    public double raizCuadrada(double numero) {
+        return Math.sqrt(numero);
+    }
 }

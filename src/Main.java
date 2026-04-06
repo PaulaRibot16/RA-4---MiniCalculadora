bb9ac9f2d4d8bc6ff5de71bf512df61d68099296
paula <pribros1503@g.educaand.es>
2026-04-06 12:36:14 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,30 @@
-public class Main {}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String menu = """
+                1. Raíz Cuadrada
+                2. Valor Absoluto
+                0. Salir 
+                """;
+
+        int opcion;
+        System.out.print("Introduce un numero a analizar: ");
+        double numero = sc.nextDouble();
+        do {
+            System.out.println(menu);
+
+            System.out.print("Introduce una opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1 -> System.out.printf("Raíz cuadrada de %.2f: %.2f%n", numero, MiniCalculadoraEjemplo.raizCuadrada(numero));
+                case 2 -> System.out.printf("Valor Absoluto de %.2f: %.2f%n", numero, MiniCalculadoraEjemplo.valorAbsoluto(numero));
+                case 0 -> System.out.println("Saliendo...");
+                default -> System.out.println("Opcion incorrecta");
+            }
+        } while (opcion != 0);
+    }
+}
--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -1,9 +1,9 @@
 public class MiniCalculadoraEjemplo {
-    public double valorAbsoluto(double numero) {
+    public static double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
 
-    public double raizCuadrada(double numero) {
+    public static double raizCuadrada(double numero) {
         return Math.sqrt(numero);
     }
 }

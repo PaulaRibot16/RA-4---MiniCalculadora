1c9d76c74ca57b217e0d187041cb0d73075e2c92
paula <pribros1503@g.educaand.es>
2026-04-06 12:53:00 +0200

    doc.valorAbsoluto

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -1,4 +1,9 @@
 public class MiniCalculadoraEjemplo {
+    /**
+     * Calcula el valor absoluto de un número double.
+     * @param numero el número a evaluar
+     * @return el valor positivo del número
+     */
     public static double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
